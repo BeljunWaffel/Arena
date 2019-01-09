ee33150e5d9f6f77592eb02eaa4f8231698daf2e
Marc-Antoine Fontenelle <ma.fontenelle@hotmail.com>
2019-01-08 22:58:43 -0800

    readding default input. Adding background + canvas for main menu

---
+++ Assets/Scripts/Game Scripts/CharacterSelector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game_Scripts
+{
+    public enum Character
+    {
+        TestCharacter
+    }
+
+    class CharacterSelector : MonoBehaviour
+    {
+        public Character Character;
+
+        private void Awake()
+        {
+            
+        }
+    }
+}
