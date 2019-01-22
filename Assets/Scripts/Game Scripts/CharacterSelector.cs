cbca44c31bf0cc068aa7573e88c5aa921556f2d7
Marc-Antoine Fontenelle <ma.fontenelle@hotmail.com>
2019-01-21 16:54:06 -0800

    Adding menu. Adding playfab services.

--- Assets/Scripts/Game Scripts/CharacterSelector.cs
+++ Assets/Scripts/Game Scripts/CharacterSelector.cs
@@ -2,18 +2,18 @@
 
 namespace Assets.Scripts.Game_Scripts
 {
-    public enum Character
-    {
-        TestCharacter
-    }
+    //public enum Character
+    //{
+    //    TestCharacter
+    //}
 
-    class CharacterSelector : MonoBehaviour
-    {
-        public Character Character;
+    //class CharacterSelector : MonoBehaviour
+    //{
+    //    public Character Character;
 
-        private void Awake()
-        {
+    //    private void Awake()
+    //    {
             
-        }
-    }
+    //    }
+    //}
 }
